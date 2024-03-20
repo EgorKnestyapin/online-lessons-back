5c37114b649251d3d926164859c43de1555a8494
Egor Knestiapin <knesp@Yandex.ru>
2024-03-20 13:04:37 +0400

    first commit

---
+++ src/main/java/de/aittr/online_lessons/OnlineLessonsApplication.java
@@ -0,0 +1,13 @@
+package de.aittr.online_lessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLessonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/aittr/online_lessons/OnlineLessonsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.aittr.online_lessons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLessonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
